66a5dd4f1cc1e075c33996c86086a0dd43da891f
SUBHAJEET DAS <125632308+subhajeet2003@users.noreply.github.com>
2025-03-21 00:02:50 +0530

    Update Project 1.sql

--- Project 1.sql
+++ Project 1.sql
@@ -86,7 +86,7 @@ SELECT COUNT(DISTINCT category) AS total_sale FROM retail_sales;
 
 -- My Analysis & Findings
 -- Q.1 Write a SQL query to retrieve all columns for sales made on '2022-11-05'.
--- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 10 in the month of Nov-2022
+-- Q.2 Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022.
 -- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
 -- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
 -- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
